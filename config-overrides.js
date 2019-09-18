81be4bf3ff67ed1a937dea8827181b40df25b470
Derek Watson <derek.watson@kiewit.com>
2019-09-18 15:42:38 -0500

    Get some Kiewit colors going

--- config-overrides.js
+++ config-overrides.js
@@ -14,7 +14,26 @@ module.exports = override(
     */
     modifyVars: { 
         // Colors
-        '@primary-color': '@red-6', // primary color
+        '@primary-color': '@yellow-6', // primary color
+
+
+        // Layout
+        '@layout-body-background': '#f0f2f5',
+        '@layout-header-background': '#000000',
+        '@layout-footer-background': '@layout-body-background',
+        '@layout-header-height': '64px',
+        '@layout-header-padding': '0 50px',
+        '@layout-footer-padding': '24px 50px',
+        '@layout-sider-background': '@layout-header-background',
+        '@layout-trigger-height': '48px',
+        '@layout-trigger-background': '#002140',
+        '@layout-trigger-color': '#fff',
+        '@layout-zero-trigger-width': '36px',
+        '@layout-zero-trigger-height': '42px',
+        // Layout light them
+        '@layout-sider-background-light': '#fff',
+        '@layout-trigger-background-light': '#fff',
+        '@layout-trigger-color-light': '@text-color',
 
         // Fonts
         '@font-family': '-apple-system, BlinkMacSystemFont, \'Source Sans Pro\', Roboto, sans-serif',
